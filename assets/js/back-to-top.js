bd51361bc09d6258d52ab8dd854ed7356d7a922e
JDroid-git <75117860+JDroid-git@users.noreply.github.com>
2022-08-05 02:20:27 +0900

    #GitPage - Modify
    - Top 버튼 생성
    - UI 수정
    - 개인, 회사 프로젝트 분리
    - 블로그 버튼 수정

---
+++ assets/js/back-to-top.js
@@ -0,0 +1,16 @@
+$(function() {
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 100) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+
+  $('#back-to-top').click(() => {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
+});
